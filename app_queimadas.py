c3623332a43a5a04ec9c1405b299675f321f27ff
geovane <geovanecarlos.miguel@gmail.com>
2025-02-20 22:49:13 -0300

    segundo commit

--- app_queimadas.py
+++ app_queimadas.py
@@ -71,15 +71,15 @@ def calcular_focos_anual(df_queimadas):
 # Função para calcular o acumulado total de focos de queimadas em Itajubá
 def calcular_sazonalidade_focos(df_queimadas):
     df_focos_totais_itajuba = df_queimadas.resample('ME')['Número de Focos'].sum().reset_index()
-    df_focos_totais_itajuba['Mês'] = df_focos_totais_itajuba['Data'].dt.strftime('%B')
-    df_focos_totais_itajuba['Ano'] = df_focos_totais_itajuba['Data'].dt.strftime('%y')
+    df_focos_totais_itajuba['Mês'] = df_focos_totais_itajuba['Data'].dt.month_name(locale="pt_BR")
+    df_focos_totais_itajuba['Ano'] = df_focos_totais_itajuba['Data'].dt.year
     df_mensal_anual = df_focos_totais_itajuba.copy()
     df_mensal_anual = df_mensal_anual[['Data', 'Mês', 'Ano', 'Número de Focos']]
     df_mensal_total = df_mensal_anual.groupby("Mês")["Número de Focos"].sum().reset_index()
 
     # Ordenando os meses na sequência correta
-    list_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
-                  'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+    list_meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+                  'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 
     df_mensal_total["Mês"] = pd.Categorical(df_mensal_total["Mês"], categories=list_meses, ordered=True)
     df_mensal_total = df_mensal_total.sort_values("Mês")
